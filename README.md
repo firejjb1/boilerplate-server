c91e9c6e12b2d0e1952389a6abbddf53e3d85347
Jian Jia <j32jia@uwaterloo.ca>
2020-10-30 13:52:05 -0400

    signup route

---
+++ README.md
@@ -0,0 +1,4 @@
+# A boilerplate node.js backend for a blog application with authentication
+setup mongoDB
+npm install
+node index.js
---
+++ models/index.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+mongoose.set("debug", true);
+mongoose.Promise = Promise;
+// "mongodb+srv://shallow:UzmHUKJFHuyBpsau@dev.v0oyv.mongodb.net/warbler?retryWrites=true&w=majority"
+mongoose.connect("mongodb://localhost/warbler", {
+  keepAlive: true
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+    console.log('connected')
+});
+module.exports.User = require("./user");
